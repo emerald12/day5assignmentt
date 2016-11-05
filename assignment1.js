610f5897983ba7c291045759de7e064853d6a8a9
emerald12 <nifemioluwadare@ymail.com>
2016-11-05 16:50:30 +0100

    Assignment5 by Tolu

---
+++ assignment1.js
@@ -0,0 +1,19 @@
+for (var i = 1; i < 20; i++)
+{
+    if (i % 3 == 0)
+    {
+       console.log ("FIZZ");        
+    }
+    else if (i % 5 == 0)
+    {
+       console.log ("BUZZ");
+    }
+    else if (i % 5 == 0) //
+ (i % 3 == 0)
+ {
+       console.log ("FIZZBUZZ");
+ }
+ else {
+     console.log ("NUMBERS");
+ }
+}
---
+++ assignment2.js
@@ -0,0 +1,19 @@
+var level = 15
+if (level >= 0) && (level <= 10){
+    console.log ("you are a level"leve"staff and your designation is casual staff");
+}
+else if (level >= 11) && (level <= 20){
+    console.log ("you are a level"level"staff and your designation is Junior staff");
+}
+else if (level >= 21) && (level <= 30){
+    console.log ("you are a level"level"staff and your designation is senior staff");
+}
+else if (level >= 31) && (level <= 40){
+    console.log ("you are a level"level"staff and your designation is manager");
+}
+else if (level >= 41) && (level <=50){
+    console.log ("you are a level"level"staff and your designation is Director");
+}
+else {
+    console.log ("invalid");
+}
